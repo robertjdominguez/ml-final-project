df49c2819e03d3040eabd52d0bd4900119d1607d
Robert Dominguez <rtjdominguez@gmail.com>
2022-05-01 19:27:31 -0500

    init

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+// the workstation div
+var workstation = document.getElementById("workstation");
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  workstation.appendChild(document.getElementById(data));
+}
